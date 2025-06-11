d7e73e342d9cb70c118713b793aeb8226e74cc63
Yasmin Fanfa <132277675+YasminTF@users.noreply.github.com>
2025-06-11 19:51:56 -0300

    update class controller

--- imperium-backend/src/main/java/com/imperium/backend/api/controller/ApiController.java
+++ imperium-backend/src/main/java/com/imperium/backend/api/controller/ApiController.java
@@ -1,8 +1,37 @@
 package com.imperium.backend.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class ApiController {
 
+    private List<String> cadastros = new ArrayList<>();
+
+    private final ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(path = "/cadastros")
+    public ResponseEntity<String> cadastroCliente() throws JsonProcessingException {
+        return ResponseEntity.ok(objectMapper.writeValueAsString(cadastros));
+    }
+
+    @PostMapping(path = "/cadastros")
+    public ResponseEntity<Void> createClient(@RequestBody String cadastro) {
+        cadastros.add(cadastro);
+        return ResponseEntity.ok().build();
+    }
 }
+
