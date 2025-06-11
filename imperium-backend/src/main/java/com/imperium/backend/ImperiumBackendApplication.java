ec00e404a9aaa29651c1f76138830295aa55871d
Yasmin Fanfa <132277675+YasminTF@users.noreply.github.com>
2025-06-10 21:44:23 -0300

    update class controller

--- imperium-backend/src/main/java/com/imperium/backend/ImperiumBackendApplication.java
+++ imperium-backend/src/main/java/com/imperium/backend/ImperiumBackendApplication.java
@@ -1,13 +1,46 @@
-package com.imperium.backend;
+package com.imperium.backend.api.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
-public class ImperiumBackendApplication {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImperiumBackendApplication.class, args);
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@RestController
+public class ApiController {
+	
+	
+	private List<String> cadastros = new ArrayList<>();
+	
+	private ObjectMapper objectMapper;
+	
+	public ApiController(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+	
+	@GetMapping(path = "/cadastros")
+	public ResponseEntity<String> cadastroCliente() throws JsonProcessingException{
+	
+		return ResponseEntity.ok(objectMapper.writeValueAsString(cadastros));
+	}
+	
+	@PostMapping(path = "/cadastros")
+	public ResponseEntity<Void> createClient(@RequestBody String cadastro){
+		cadastros.add(cadastro);
+		return ResponseEntity.ok().build();
+				
+	}
+	
+	public ApiController(List<String> cadastro) {
+		super();
+		this.cadastros = cadastro;
 	}
 
 }
